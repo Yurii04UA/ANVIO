76b9566661d3dc1d0076bc110a78d42d6ccb16c7
Yurii04UA <uyriparaka@gmail.com>
2022-08-12 00:22:28 +0300

    feat: added some logic for behavior modal window

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const cross = document.querySelector(".header-mobile_cross");
 const modalWindow1 = document.querySelector(".mobile-menu-modal-1");
 const modalWindow2 = document.querySelector(".mobile-menu-modal-2");
 const modalWindow3 = document.querySelector(".mobile-menu-modal-3");
+
+const body = document.body;
 const arrayModals = [
   ,
   document.querySelector(".mobile-menu-modal-1"),
@@ -13,7 +15,7 @@ const menuButtons = document.querySelectorAll(".menu-button");
 
 menuButtons.forEach((elem) => {
   elem.addEventListener("click", () => {
-   // add/delete active circle
+    // add/delete active circle
     menuButtons.forEach((e) => {
       if (e.id != elem.id) {
         e.classList.remove("mobile-menu-active");
@@ -27,13 +29,16 @@ menuButtons.forEach((elem) => {
         e.classList.remove(`show-modal`);
       }
     });
+
     arrayModals[elem.id].classList.toggle(`show-modal`);
+
+    if (arrayModals[elem.id].classList.value.includes(`show-modal`)) {
+      body.classList.add("OH");
+    } else {
+      body.classList.remove("OH");
+    }
   });
 });
-
-
-
-
 
 // menu.addEventListener("click", () => {
 //   cross.style.display = "flex";
